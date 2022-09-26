e8407feeceb87b288b21b05dfb5ca90db638b67a
Jon Dahl <jon@dira.no>
2022-09-26 22:13:45 +0200

    Add PyPi requirements

---
+++ kshell_py/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.0.0.0"
+__author__ = "Jon Kristian Dahl"
+__credits__ = "Noritaka Shimizu"
+
+"""
+Version legend:
+a.b.c.d
+
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
+from .kshell_py import *
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+"""
+Version legend:
+a.b.c.d
+
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
+setup(
+    name = 'kshell-py',
+    version = '0.0.0.0',
+    description = 'Python implementation of the nuclear shell model solver, KSHELL.',
+    url = 'https://github.com/GaffaSnobb/kshell-py',
+    author = 'Jon Kristian Dahl',
+    author_email = 'jonkd@uio.no',
+    packages = ['kshell_py', 'test'],
+    install_requires = ['numpy'],
+
+    classifiers = [
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+)
