8d40486299f8a8b4a354723ba40151e2a4181df4
Jon Dahl <jon@dira.no>
2022-11-02 18:11:47 +0100

    In the middle of implementing m-scheme bit representation

--- kshell_py/partition.py
+++ kshell_py/partition.py
@@ -1,6 +1,6 @@
 import os, sys, time
 from parameters import timing, flags
-from data_structures import Partition, Interaction, ModelSpace
+from data_structures import Partition, Interaction, ModelSpace, BitRepresentation
 from loaders import read_partition_file
 import numpy as np
 
@@ -72,29 +72,13 @@ def initialise_partition(
         model_space = interaction.neutron_model_space
     )
 
-    for i in range(partition.n_proton_configurations):
-        """
-        Initialise min_m and max_m for protons.
-        """
-        partition.proton_configurations_min_max[i, 0] = partition.proton_configurations_max_j[i]
-        partition.proton_configurations_min_max[i, 1] = -partition.proton_configurations_max_j[i]
-
-    for i in range(partition.n_neutron_configurations):
-        """
-        Initialise min_m and max_m for neutrons.
-        """
-        partition.neutron_configurations_min_max[i, 0] = partition.neutron_configurations_max_j[i]
-        partition.neutron_configurations_min_max[i, 1] = -partition.neutron_configurations_max_j[i]
-
-    partition.max_proton_neutron_couple_j = 0  # The maximum possible angular momentum of the system.
+    partition.max_proton_neutron_couple_j = 0  # The maximum possible angular momentum of the system ('max_jj' in the Fortran code).
     for i in range(partition.n_proton_neutron_configurations):
         """
-        Loop over proton-neutron configurations. NOTE: I do not know
-        what the min_max values are for yet. If the final min value is
-        always -max, then why bother storing / calcuating both?
-
-        In this loop, the maximum possible angular momentum of all
-        proton-neutron configurations is calculated.
+        Loop over proton-neutron configurations and calculate the
+        maximum possible angular momentum of all proton-neutron
+        configurations. In the Fortran code:
+        https://github.com/GaffaSnobb/kshell/blob/088e6b98d273a4b59d0e2d6d6348ec1012f8780c/src/partition.F90#L158
         """
         proton_idx: int = partition.proton_neutron_configurations[i, 0]
         neutron_idx: int = partition.proton_neutron_configurations[i, 1]
@@ -105,34 +89,36 @@ def initialise_partition(
         partition.max_proton_neutron_couple_j = max(  # Find the combination of proton-neutron configurations with the largest angular momentum.
             proton_max_j + neutron_max_j, partition.max_proton_neutron_couple_j
         )
-        partition.proton_configurations_min_max[proton_idx, 0] = min(
-            partition.proton_configurations_min_max[proton_idx, 0],
-            partition.m_tot - neutron_max_j
-        )
-        partition.neutron_configurations_min_max[neutron_idx, 0] = min(
-            partition.neutron_configurations_min_max[neutron_idx, 0],
-            partition.m_tot - proton_max_j
-        )
-        partition.proton_configurations_min_max[proton_idx, 1] = max(
-            partition.proton_configurations_min_max[proton_idx, 1],
-            partition.m_tot + neutron_max_j
-        )
-        partition.neutron_configurations_min_max[neutron_idx, 1] = max(
-            partition.neutron_configurations_min_max[neutron_idx, 1],
-            partition.m_tot + proton_max_j
+
+    for i in range(partition.n_proton_configurations):
+        """
+        Loop over proton configurations and calculate the possible jz
+        values for each configuration. These loops replace the code
+        https://github.com/GaffaSnobb/kshell/blob/088e6b98d273a4b59d0e2d6d6348ec1012f8780c/src/partition.F90#L159-L170
+        and
+        https://github.com/GaffaSnobb/kshell/blob/088e6b98d273a4b59d0e2d6d6348ec1012f8780c/src/partition.F90#L180-L184
+        """
+        partition.proton_configurations_jz[i] = np.arange(
+            start = -partition.proton_configurations_max_j[i],
+            stop = partition.proton_configurations_max_j[i] + 1,    # +1 to include the last value.
+            step = 1
         )
 
-    print(interaction.proton_model_space.j)
-    print(partition.proton_configurations[:, 1:])
-    print(partition.proton_configurations_max_j)
-    
-    # for i in range(partition.n_proton_configurations):
-    #     mi = max(-partition.proton_configurations_max_j[i], partition.proton_configurations_min_max[i, 0])
-    #     mj = min( partition.proton_configurations_max_j[i], partition.proton_configurations_min_max[i, 1])
+    for i in range(partition.n_neutron_configurations):
+        """
+        Loop over neutron configurations and calculate the possible jz
+        values for each configuration.
+        """
+        partition.neutron_configurations_jz[i] = np.arange(
+            start = -partition.neutron_configurations_max_j[i],
+            stop = partition.neutron_configurations_max_j[i] + 1,   # +1 to include the last value.
+            step = 1
+        )
 
-    #     print(f"{mi = }")
-    #     print(f"{partition.proton_configurations_min_max[i] = }")
-
+    initialise_m_scheme_bit_representation(
+        interaction = interaction,
+        partition = partition,
+    )
 
     initialise_partition_time = time.perf_counter() - initialise_partition_time
     timing.initialise_partition_time = initialise_partition_time
@@ -140,6 +126,131 @@ def initialise_partition(
         print(f"initialise_partition_time: {initialise_partition_time:.4f} s")
 
     return partition
+
+def initialise_m_scheme_bit_representation(
+    interaction: Interaction,
+    partition: Partition
+):
+    """
+    type type_m_mbit 
+        integer :: n 
+        integer(kmbit), allocatable :: mbit(:)
+        integer, allocatable :: mm(:)
+    end type type_m_mbit
+    """
+    initialise_m_scheme_bit_representation_time: float = time.perf_counter()
+
+    mbit_orb: np.ndarray = np.zeros(
+        shape = (
+            max(interaction.model_space.n_proton_orbitals, interaction.model_space.n_neutron_orbitals),
+            max(partition.n_protons, partition.n_neutrons) + 1,
+            2   # 0 is proton, 1 is neutron.
+        ),
+        dtype = BitRepresentation
+    )
+
+    for i in range(mbit_orb.shape[0]):
+        """
+        Initialise all elements in mbit_orb.
+        """
+        for j in range(mbit_orb.shape[1]):
+            for k in range(mbit_orb.shape[2]):
+                mbit_orb[i, j, k] = BitRepresentation(n=0)
+
+    for loop in range(2):
+        """
+        get rid of this stupid loop
+        """
+
+        for i in range(mbit_orb.shape[0]):
+            for j in range(mbit_orb.shape[1]):
+                for k in range(mbit_orb.shape[2]):
+                    mbit_orb[i, j, k].n = 0
+
+        for orbital_idx in range(interaction.proton_model_space.n_orbitals):
+            for mm in range(2**(interaction.proton_model_space.j[orbital_idx] + 1) - 1 + 1):
+                """
+                Calculate the population count (popcnt, bit count) of mm. If
+                the popcnt of mm is <= the number of valence protons
+                (neutrons) then the current [orbital, popcnt, ipn] is
+                counted (n += 1).
+                
+                Example (Ne20_usda):
+                For angular momentum 3, we have mm and corresponding popcnt:
+                
+                mm     = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+                popcnt = 0, 1, 1, 2, 1, 2, 2, 3, 1, 2,  2,  3,  2,  3,  3,  4
+
+                For Ne20_usda we have 2 valence protons meaning that we are
+                left with
+
+                mm     = 0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12
+                popcnt = 0, 1, 1, 2, 1, 2, 2, 1, 2,  2,  2
+                mm_removed = 7, 11, 13, 14, 15
+
+                and consequently, n is incremented 11 times (not at the same
+                index every time, mind you).
+                
+                """
+                # print(f"{mm = }")
+                popcnt = mm.bit_count()  # Count the number of bits which are set to 1.
+
+                if popcnt > partition.n_protons:
+                    """
+                    When the number of bits set to 1 in mm is greater than
+                    the number of valence protons... Why should this happen?
+
+                    Well for starters, this avoids IndexError in mbit_orb a
+                    few lines down from here.
+                    """
+                    continue
+
+                mbit_orb[orbital_idx, popcnt, 0].n += 1
+                
+                if loop == 1:
+                    n_tmp: int = mbit_orb[orbital_idx, popcnt, 0].n - 1  # Think I have to subtract 1 to get 0-based indexing.
+                    if n_tmp == -1:
+                        msg = "n_tmp should not be -1!"
+                        raise RuntimeError(msg)
+
+                    try:
+                        mbit_orb[orbital_idx, popcnt, 0].mbit[n_tmp] = mm
+                    except IndexError as e:
+                        print(f"{orbital_idx = }")
+                        print(f"{popcnt = }")
+                        print(f"{mm = }")
+                        print(f"{n_tmp = }")
+                        raise(e)
+
+        if loop == 1: continue
+        
+        for orbital_idx in range(interaction.proton_model_space.n_orbitals):
+            for nucleon_idx in range(partition.n_protons + 1):
+                
+                n_tmp: int = mbit_orb[orbital_idx, nucleon_idx, 0].n
+                if n_tmp == 0:
+                    msg = "n_tmp should not be zero!"
+                    raise RuntimeError(msg)
+                
+                mbit_orb[orbital_idx, nucleon_idx, 0].mbit = np.zeros(
+                    shape = n_tmp,
+                    dtype = int
+                )
+                mbit_orb[orbital_idx, nucleon_idx, 0].mm = np.zeros(
+                    shape = n_tmp,
+                    dtype = int
+                )
+    
+    for i in range(mbit_orb.shape[0]):
+        for j in range(mbit_orb.shape[1]):
+            for k in range(mbit_orb.shape[2]):
+                print(mbit_orb[i, j, k].n)
+
+    initialise_m_scheme_bit_representation_time = time.perf_counter() - initialise_m_scheme_bit_representation_time
+    timing.initialise_m_scheme_bit_representation_time = initialise_m_scheme_bit_representation_time
+    if flags.debug:
+        print(f"initialise_m_scheme_bit_representation_time: {initialise_m_scheme_bit_representation_time:.4f} s")
+
 
 def calculate_configuration_parity(
     configurations: np.ndarray,
