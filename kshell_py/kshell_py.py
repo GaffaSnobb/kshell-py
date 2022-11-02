47e16061c5cdb4d095cc13593b92d1251486d6d3
Jon Dahl <jon@dira.no>
2022-11-02 18:10:37 +0100

    Minor changes to interface file

--- kshell_py/kshell_py.py
+++ kshell_py/kshell_py.py
@@ -1,21 +1,35 @@
 from parameters import flags, timing
 from operator_j_scheme import operator_j_scheme
-from data_structures import Interaction, OperatorJ
+from data_structures import Interaction, OperatorJ, Partition
 from loaders import read_interaction_file
 from partition import initialise_partition
 flags.debug = True
 flags.timing_summary = False
 
-def main(path="../snt/usda.snt"):
-    interaction: Interaction = read_interaction_file(path=path)
-    operator_j: OperatorJ = operator_j_scheme(
-        interaction = interaction,
-        nucleus_mass = 20
-    )
-    initialise_partition(path="../ptn/Ne20_usda_p.ptn")
+def main(path_interaction: str, path_partition: str):
+    interaction: Interaction = read_interaction_file(path=path_interaction)
+    partition: Partition = initialise_partition(path=path_partition, interaction=interaction)
+    # operator_j: OperatorJ = operator_j_scheme(
+    #     interaction = interaction,
+    #     nucleus_mass = 20
+    # )
     
     if flags.timing_summary:
         print(timing)
 
 if __name__ == "__main__":
-    main()
+    path_interaction_usda = "../snt/usda.snt"
+    path_partition_usda = "../ptn/Ne20_usda_p.ptn"
+    
+    path_interaction_sdpfmu = "../snt/sdpf-mu.snt"
+    path_partition_sdpfmu = "../ptn/V50_sdpf-mu_n.ptn"
+    
+    main(
+        path_interaction = path_interaction_usda,
+        path_partition = path_partition_usda
+    )
+
+    # main(
+    #     path_interaction = path_interaction_sdpfmu,
+    #     path_partition = path_partition_sdpfmu
+    # )
