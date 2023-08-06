4d244a84c8e5e46b034af092e9e79678454240e5
Jon Dahl <jon@dira.no>
2023-08-06 20:00:34 +0900

    Start implementation of general hamiltonian

--- kshell_py/basic_solver.py
+++ kshell_py/basic_solver.py
@@ -1,13 +1,17 @@
 from typing import Any
 import numpy as np
 import numpy.linalg as lalg
-from kshell_utilities.data_structures import Interaction
+import kshell_utilities as ksutil
+from kshell_utilities.data_structures import Interaction, Partition
 from kshell_utilities.loaders import load_interaction, load_partition
 
-def construct_hamiltonian(
-    interaction: Interaction
-) -> np.ndarray[Any, float]:
-    
+def O18_w_manual_hamiltonian() -> np.ndarray[Any, float]:
+    """
+    Manual construction of the shell model hamiltonian for 18O (2
+    valence protons) with the w interaction.
+    """
+    O18 = ksutil.loadtxt(path="tmp_delete/")
+    interaction: Interaction = load_interaction(filename_interaction="tmp_delete/w.snt")
     spe = interaction.spe
 
     def tbme(a, b, c, d, j):
@@ -23,20 +27,54 @@ def construct_hamiltonian(
         [           tbme(0, 2, 0, 0, 0),            tbme(0, 2, 1, 1, 0),            tbme(0, 2, 2, 2, 0),                   tbme(0, 2, 0, 1, 0), spe[0] + spe[2] + tbme(0, 2, 0, 2, 0),                   tbme(0, 2, 1, 2, 0)], # d3/2 s1/2
         [           tbme(1, 2, 0, 0, 0),            tbme(1, 2, 1, 1, 0),            tbme(1, 2, 2, 2, 0),                   tbme(1, 2, 0, 1, 0),                   tbme(1, 2, 0, 2, 0), spe[1] + spe[2] + tbme(1, 2, 1, 2, 0)], # d5/2 s1/2
     ])
+    
+    assert np.all(H == H.T)
+
+    eigenvalues, eigenvectors = lalg.eigh(H)
+    # print(H)
+    print(eigenvalues)
+    print(O18.levels)
+    # print(eigenvectors)
 
     return H
+
+def calculate_hamiltonian_dimension(
+    interaction: Interaction,
+    partition_proton: Partition,
+    partition_neutron: Partition,
+    partition_combined: Partition,
+) -> tuple[int, int]:
+
+    print(partition_proton.configurations)
+    # for orbital in interaction.model_space.orbitals:
+    #     orbital.degeneracy
+
+    return 0, 0
+
+def create_hamiltonian(
+    interaction: Interaction,
+    partition_proton: Partition,
+    partition_neutron: Partition,
+    partition_combined: Partition,
+):
+    n_rows, n_cols = calculate_hamiltonian_dimension(
+        interaction = interaction,
+        partition_proton = partition_proton,
+        partition_neutron = partition_neutron,
+        partition_combined = partition_combined,
+    )
 
 def main():
     interaction: Interaction = load_interaction(filename_interaction="tmp_delete/w.snt")
     partition_proton, partition_neutron, partition_combined = \
         load_partition(filename_partition="tmp_delete/O18_w_p.ptn", interaction=interaction)
     
-
-
-
-    H: np.ndarray = construct_hamiltonian(interaction=interaction)
-    print(H)
-    # eigenvalues, eigenvectors = lalg.eigh(H)
+    create_hamiltonian(
+        interaction = interaction,
+        partition_proton = partition_proton,
+        partition_neutron = partition_neutron,
+        partition_combined = partition_combined,
+    )
 
 if __name__ == "__main__":
     main()
