a9c427f0c1047d7f24dba5d501abcacec6544b34
Jon Dahl <jon@dira.no>
2023-08-30 16:56:14 +0900

    Add test for calculate_all_possible_pairs

--- basic_solver/test_partition.py
+++ basic_solver/test_partition.py
@@ -1,10 +1,17 @@
 # from kshell_utilities.data_structures import Partition, Interaction
 import kshell_utilities.data_structures as ksutild
 from kshell_utilities.loaders import load_interaction, load_partition
-from partition import calculate_all_possible_orbital_occupations
 from data_structures import Partition
+from partition import (
+    calculate_all_possible_orbital_occupations, calculate_all_possible_pairs
+)
 
 def test_calculate_all_possible_orbital_occupations():
+    """
+    Test that the function calculate_all_possible_orbital_occupations
+    creates O19 neutron partition identical to the O19 partition file
+    from KSHELL.
+    """
     interaction: ksutild.Interaction = load_interaction(filename_interaction="../snt/w.snt")
     n_valence_protons = 0
     n_valence_neutrons = 3
@@ -24,6 +31,33 @@ def test_calculate_all_possible_orbital_occupations():
         calculated = calculated.configuration
         assert all(expected_occ == calculated_occ for expected_occ, calculated_occ in zip(expected, calculated))
 
+def test_calculate_all_possible_pairs():
+    """
+    Test that calculate_all_possible_pairs manages to calculate all
+    possible pairs from a bunch of configurations.
+    """
+    configurations: list[tuple[int, ...]] = [
+        (0, 1, 2), (0, 2, 1), (0, 3, 0), (1, 0, 2), (1, 1, 1), (1, 2, 0),
+        (2, 0, 1), (2, 1, 0), (3, 0, 0)
+    ]
+    expected: list[list[tuple[int, ...]]] = [
+        [(1, 2), (1, 2), (2, 2)], [(1, 1), (1, 2), (1, 2)],
+        [(1, 1), (1, 1), (1, 1)], [(0, 2), (0, 2), (2, 2)],
+        [(0, 1), (0, 2), (1, 2)], [(0, 1), (0, 1), (1, 1)],
+        [(0, 0), (0, 2), (0, 2)], [(0, 0), (0, 1), (0, 1)],
+        [(0, 0), (0, 0), (0, 0)],
+
+    ]
+    expected = [sorted(i) for i in expected]
+
+    for i in range(len(configurations)):
+
+        calculated = calculate_all_possible_pairs(
+            configuration = configurations[i],
+            M_target = 1,
+        )
+        assert calculated == expected[i], f"{calculated} != {expected[i]}"
 
 if __name__ == "__main__":
     test_calculate_all_possible_orbital_occupations()
+    test_calculate_all_possible_pairs()
