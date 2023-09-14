80caf39c6fecfc4891251334f91e9b6399f6ccc4
Jon Dahl <jon@dira.no>
2023-09-14 18:47:14 +0900

    Calculate m substate indices
    Calculate all the possible ways of arranging the valence nucleons so that their summed m values are equal to M_target.

--- basic_solver/interaction.py
+++ basic_solver/interaction.py
@@ -1,15 +1,62 @@
+from itertools import combinations
 from kshell_utilities.data_structures import Interaction
 
-def fill_orbital_m_substates(
+def calculate_m_substates(
     interaction: Interaction,
-):
-    # m_substates: list[int] = []
+    M_target: int,
+) -> tuple[tuple[int, ...], ...]:
+    """
+    Given a model space and a number of valence nucleons, calculate all
+    possible combinations of nucleons whose magnetic substates sum up
+    to `M_target`. Consider the following figure which shows the sd
+    model space. Note that the ordering of the orbitals is the same
+    order as they appear in usda.snt.
 
-    m_substates: tuple[int] = (1,)
+                  10    11        
+                -  O  -  O  -             s1/2: 2
+                 -1/2   1/2
 
-    for orb in interaction.model_space_neutron.orbitals:
-        print(f"{orb.jz = }")
+       4     5     6     7     8     9    
+    -  O  -  O  -  O  -  O  -  O  -  O    d5/2: 1
+     -5/2  -3/2  -1/2   1/2   3/2   5/2
 
-    print(m_substates)
+             0     1     2     3
+          -  O  -  O  -  O  -  O  -       d3/2: 0
+           -3/2  -1/2   1/2   3/2
 
-    # print(interaction.model_space_neutron.orbitals[2].jz)
+    This function will iterate over all the possible configurations of
+    the valence particles and save the configurations whose m values sum
+    up to M_target.
+
+    NOTE: There are good possibilities of parallelising the contents of
+    this function, should it be needed.
+
+    Returns
+    -------
+    index_combinations_filtered : tuple[tuple[int, ...], ...]
+        All the possible ways to combine N particles in the given model
+        space as to produce M = M_target. Each combination is stored as
+        a tuple of N m-substate indices, and each of the tuples are
+        nested in a tuple.
+    """
+    all_m_values: tuple[int, ...] = tuple()
+
+    for orbital in interaction.model_space_neutron.orbitals:
+        """
+        Gather all m (jz) values in a single tuple.
+        """
+        all_m_values += orbital.jz
+
+    # Generate all combinations of m value indices of length n_valence_nucleons.
+    index_combinations = combinations(range(len(all_m_values)), interaction.model_space_neutron.n_valence_nucleons)
+    
+    # Filter combinations so that combinations with m_0 + m_1 + ... == M_target are kept.
+    index_combinations_filtered = filter(
+        lambda indices: sum(all_m_values[i] for i in indices) == M_target,
+        index_combinations,
+    )
+
+    print(f"{all_m_values = }")
+
+    return tuple(index_combinations_filtered)
+    
