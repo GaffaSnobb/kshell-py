34ff148d2f1ebfaf309d903834cb3b75664a918f
Jon Dahl <jon@dira.no>
2023-10-13 14:02:28 +0900

    Include both proton and neutron orbitals

--- basic_solver/basis.py
+++ basic_solver/basis.py
@@ -16,16 +16,28 @@ def calculate_m_basis_states(
     model space. Note that the ordering of the orbitals is the same
     order as they appear in usda.snt.
 
+                  22    23        
+                -  O  -  O  -             neutron s1/2: 5
+                 -1/2   1/2
+
+      16    17    18    19    20    21    
+    -  O  -  O  -  O  -  O  -  O  -  O    neutron d5/2: 4
+     -5/2  -3/2  -1/2   1/2   3/2   5/2
+
+            12    13    14    15
+          -  O  -  O  -  O  -  O  -       neutron d3/2: 3
+           -3/2  -1/2   1/2   3/2
+
                   10    11        
-                -  O  -  O  -             s1/2: 2
+                -  O  -  O  -             proton s1/2: 2
                  -1/2   1/2
 
        4     5     6     7     8     9    
-    -  O  -  O  -  O  -  O  -  O  -  O    d5/2: 1
+    -  O  -  O  -  O  -  O  -  O  -  O    proton d5/2: 1
      -5/2  -3/2  -1/2   1/2   3/2   5/2
 
              0     1     2     3
-          -  O  -  O  -  O  -  O  -       d3/2: 0
+          -  O  -  O  -  O  -  O  -       proton d3/2: 0
            -3/2  -1/2   1/2   3/2
 
     This function will iterate over all the possible configurations of
@@ -37,26 +49,36 @@ def calculate_m_basis_states(
 
     Returns
     -------
-    index_combinations_filtered : tuple[tuple[int, ...], ...]
+    basis_states : tuple[tuple[int, ...], ...]
         All the possible ways to combine N particles in the given model
         space as to produce M = M_target. Each combination is stored as
         a tuple of N m-substate indices, and each of the tuples are
         nested in a tuple.
     """
-
     timing = time.perf_counter()
+    n_proton_m_substates: int = len(interaction.model_space_proton.all_jz_values)
+    n_neutron_m_substates: int = len(interaction.model_space_neutron.all_jz_values)
+
     # Generate all combinations of m substate indices of length n_valence_nucleons.
-    index_combinations = combinations(
-        iterable = range(len(interaction.model_space_neutron.all_jz_values)),
-        r = interaction.model_space_neutron.n_valence_nucleons,
+    proton_index_combinations = combinations(
+        iterable = range(n_proton_m_substates),
+        r = interaction.model_space_proton.n_valence_nucleons,
     )
+    neutron_index_combinations = tuple(combinations(    # One of these generators must be converted to a list / tuple, else they will get exhausted in the first iteration of the combined generator.
+        iterable = range(
+            n_proton_m_substates,
+            n_proton_m_substates + n_neutron_m_substates,
+        ),
+        r = interaction.model_space_neutron.n_valence_nucleons,
+    ))
+    combined_index_combinations = ((p + n) for p in proton_index_combinations for n in neutron_index_combinations)
     
     # Filter combinations so that combinations with m_0 + m_1 + ... == M_target are kept.
-    index_combinations_filtered = filter(
-        lambda indices: sum(interaction.model_space_neutron.all_jz_values[i] for i in indices) == M_target,
-        index_combinations,
-    )
+    basis_states = tuple(filter(
+        lambda indices: sum(interaction.model_space.all_jz_values[i] for i in indices) == M_target,
+        combined_index_combinations,
+    ))
     timing = time.perf_counter() - timing
     timings.calculate_m_basis_states_002 = timing
-    return tuple(index_combinations_filtered)
-    # return index_combinations_filtered
+
+    return basis_states
