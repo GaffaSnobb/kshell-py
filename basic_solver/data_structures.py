b61145d546f2da20c935773bc1f4aedb0933639a
Jon Dahl <jon@dira.no>
2023-10-13 18:51:39 +0900

    Extend the time print functionality
    This just turned into a mess... May be working now, lets see.

--- basic_solver/data_structures.py
+++ basic_solver/data_structures.py
@@ -82,76 +82,153 @@ class Interaction:
     tbme: dict[tuple[int, int, int, int, int], float]
 
 @dataclass(slots=True)
+class TimingGroupID:
+    time: float
+    group_ids: list[int]
+    attr_ids: list[int]
+
+# @dataclass(slots=True)
 class Timing:
     """
     All timing values are in seconds and are named identically as the
     function they represent.
     """
-    # fill_orbitals: float = -1.0
-    # calculate_all_possible_orbital_occupations: float = -1.0
-    # calculate_all_possible_pairs: float = -1.0
-    calculate_onebody_matrix_element_003: float = 0.0
-    calculate_twobody_matrix_element_004: float = 0.0
-    generate_indices_001: float = 0.0
-    calculate_m_basis_states_002: float = 0.0
-    create_hamiltonian_000: float = 0.0
-    main: float = 0.0
-
+    def __init__(self) -> None:
+        self.calculate_onebody_matrix_element = TimingGroupID(
+            time = 0.0,
+            group_ids = [0],
+            attr_ids = [3],
+        )
+        self.calculate_twobody_matrix_element = TimingGroupID(
+            time = 0.0,
+            group_ids = [0, 0],
+            attr_ids = [4, 0],
+        )
+        self.calculate_twobody_matrix_element_sub = TimingGroupID(
+            time = .2,
+            group_ids = [0, 0],
+            attr_ids = [4, 1],
+        )
+        self.generate_indices = TimingGroupID(
+            time = 0.0,
+            group_ids = [0],
+            attr_ids = [1],
+        )
+        self.calculate_m_basis_states = TimingGroupID(
+            time = 0.0,
+            group_ids = [0],
+            attr_ids = [2],
+        )
+        self.create_hamiltonian = TimingGroupID(
+            time = 0.0,
+            group_ids = [0],
+            attr_ids = [0],
+        )
+        self.total = TimingGroupID(
+            time = 0.0,
+            group_ids = [],
+            attr_ids = [0],
+        )
 
     def __str__(self):
         """
         Return a nicely formatted time table.
         """
-        groups = [0]            # The group IDs to be included in the table.
         group_total_sum = 0.0   # The sum of all the group times (total program time).
         TIME_TOL = 1e-3         # Tolerance for the assertions.
         # total = sum(getattr(self, attr) for attr in self.__slots__ if ((getattr(self, attr) != 0) and (attr != "main")))
+        defined_group_ids = [0]    # The group IDs to be included in the table.
 
         time_summary: str = "------------\n"
 
-        for group in groups:
+        group_timings: dict[int, list[tuple[int, str, float]]] = {}
+        sub_group_timings: dict[tuple[int, int], list[tuple[str, float]]] = {}
+        
+        for defined_group in defined_group_ids:
             """
-            Timings are collected in groups. The attr in a group with
-            attr ID 0 is the 'head' of the group and all other attrs in
-            the same group should sum up to attr ID 0's time.
+            Loop over all defined groups.
             """
-            group_timings: list[tuple[str, int]] = []
+            group_timings[defined_group] = []
+            group_total: float = 0.0
+            sub_group_total: float = 0.0
             
-            for attr in sorted(self.__slots__):
+            for attr_name in sorted([attr_name for attr_name in dir(self) if not attr_name.startswith('__')]):
                 """
-                Fetch the attrs and their values for the current group.
+                Loop over all non-special attributes.
                 """
-                if attr == "main":
+                if attr_name == "total":
                     """
-                    Treat the main time separately.
+                    The total time will be separately treated.
                     """
                     continue
                 
-                attr_id = attr.split('_')[-1]
-                group_id = int(attr_id[0])
-                attr_id = int(attr_id[1:])
-                if group_id != group: continue
+                val: TimingGroupID = getattr(self, attr_name)
+
+                time = val.time
+                group_ids = val.group_ids
+                attr_ids = val.attr_ids
+
+                if len(group_ids) == 2:
+                    try:
+                        sub_group_timings[(0, 4)].append((attr_ids[1], attr_name, time))
+                    except KeyError:
+                        sub_group_timings[(0, 4)] = []
+                        sub_group_timings[(0, 4)].append((attr_ids[1], attr_name, time))
+
+                    sub_group_total += time
+                    if attr_ids[1] != 0:
+                        """
+                        We want to include the sub-group parents in the
+                        main group overview. Sub-group children however,
+                        are not included.
+                        """
+                        continue
+
+                if group_ids[0] != defined_group:
+                    """
+                    For each attribute, group_ids[0] is the ID of the
+                    main group.
+                    """
+                    continue
                 
-                val = getattr(self, attr)
-                group_timings.insert(attr_id, (attr, val))
+                if attr_ids[0] != 0:
+                    """
+                    attr_ids[0] is the parent of the group which
+                    contains function calls of functions which contain
+                    their own timings. Dont include the parent in the
+                    sum since the time sum of the children should equal
+                    the time of the parent, minus the overhead of the
+                    parent.
+                    """
+                    group_total += time
 
-            group_total = 0.0
-            for attr, val in group_timings[1:]:
+                # group_timings[defined_group].insert(attr_ids[0], (attr_name, time))
+                group_timings[defined_group].append((attr_ids[0], attr_name, time))
+
+            group_timings[defined_group].sort(key=lambda tup: tup[0])
+
+            group_timings[defined_group].insert(    # This is the overhead time of the parent.
+                1,
+                (1, group_timings[defined_group][0][1], group_timings[defined_group][0][2] - group_total)
+            )
+            try:
                 """
-                Sum up all the times of a group, except for the head of
-                the group which should be the total group time.
+                Currently hard-coded for sub group 0.
                 """
-                group_total += val
+                group_timings[0].sort(key=lambda tup: tup[0])
+                sub_group_timings[0].insert(    # This is the overhead time of the parent.
+                    0,
+                    (1, sub_group_timings[0] [0][1], sub_group_timings[0] [0][2] - sub_group_total)
+                )
+                assert sub_group_timings[0] [1][2] >= 0  # Should never be negative.
+            except KeyError:
+                pass
 
-            # if abs(group_total - group_timings[0][1]) < TIME_TOL:
-            #     warnings.warn(f"Times for group {group} do not add up!", RuntimeWarning)
-            # assert abs(group_total - group_timings[0][1]) < TIME_TOL, f"Times for group {group} do not add up!"
-            group_total_sum += group_timings[0][1]
-            group_timings.insert(1, (group_timings[0][0], abs(group_timings[0][1] - group_total)))  # Insert the overhead of the head of the group as the second element.
+            assert group_timings[defined_group][1][2] >= 0  # Should never be negative.
 
-            for i in range(len(group_timings)):
-                attr, val = group_timings[i]
-                attr = attr[:-4]    # Remove the ID from the name.
+        for defined_group in defined_group_ids:
+            for i, group_timing in enumerate(group_timings[defined_group]):
+                attr_id, attr_name, time = group_timing
 
                 if i != 0:
                     """
@@ -159,16 +236,91 @@ class Timing:
                     """
                     time_summary += "    "
 
-                time_summary += f"{val*100/group_total:4.0f}% {val:10.4f}s: {attr}\n"
+                time_summary += f"{time*100/group_total:4.0f}% {time:10.4f}s: {attr_name}\n"
+                
+                try:
+                    sub_group_timings[(defined_group, attr_id)]
+                except KeyError:
+                    pass
+                else:
+                    for j, sub_group_timing in enumerate(sub_group_timings[(defined_group, attr_id)]):
+                        sub_attr_id, sub_attr_name, sub_time = sub_group_timing
+                        
+                        if j != 0:
+                            """
+                            Make the sub-times indented.
+                            """
+                        time_summary += "        "
 
-        # time_summary += f"{total:10.4f} s, {total/total*100:4.0f} %: total\n"
+                        time_summary += f"{sub_time*100/sub_group_total:4.0f}% {sub_time:10.4f}s: {sub_attr_name}\n"
+
+
+
+
+
+
+
+                
+
+        # for group in groups:
+        #     """
+        #     Timings are collected in groups. The attr in a group with
+        #     attr ID 0 is the 'head' of the group and all other attrs in
+        #     the same group should sum up to attr ID 0's time.
+        #     """
+        #     group_timings: list[tuple[str, int]] = []
+        #     sub_group_timings = {}
+        #     for attr in sorted([attr for attr in dir(self) if not attr.startswith('__')]):
+        #         """
+        #         Fetch the attrs and their values for the current group.
+        #         """
+        #         if attr == "main":
+        #             """
+        #             Treat the main time separately.
+        #             """
+        #             continue
+                
+        #         val = getattr(self, attr)
+        #         time = val.time
+        #         group_id_main_group = val.group_id[0]
+        #         attr_id_main_group = val.attr_id[0]
+        #         if group_id_main_group != group: continue
+
+        #         if len(val.group_id) == 2:
+        #             attr_id_sub_group = val.attr_id[1]
+        #             try:
+        #                 sub_group_timings[(group_id_main_group, attr_id_main_group)].append()
+        #             except KeyError:
+        #                 sub_group_timings[(group_id_main_group, attr_id_main_group)] = []
+        #                 sub_group_timings[(group_id_main_group, attr_id_main_group)].append()
+                
+        #         group_timings.insert(attr_id_main_group, (attr, time))
+
+        #     group_total = 0.0
+        #     for attr, val in group_timings[1:]:
+        #         """
+        #         Sum up all the times of a group, except for the head of
+        #         the group which should be the total group time.
+        #         """
+        #         group_total += val
+
+        #     group_total_sum += group_timings[0][1]
+        #     group_timings.insert(1, (group_timings[0][0], abs(group_timings[0][1] - group_total)))  # Insert the overhead of the head of the group as the second element.
+
+        #     for i in range(len(group_timings)):
+        #         attr, val = group_timings[i]
+
+        #         if i != 0:
+        #             """
+        #             Make the sub-times indented.
+        #             """
+        #             time_summary += "    "
+
+        #         time_summary += f"{val*100/group_total:4.0f}% {val:10.4f}s: {attr}\n"
+
         time_summary += "------------"
 
-        # if abs(group_total_sum - self.main) < TIME_TOL:
-        #     warnings.warn("Times do not add up! Get yo shit together!", RuntimeWarning)
-
         return time_summary
-
 
 timings: Timing = Timing()
 
