a382064006aff3ad23ea876df791d17bb3c0af59
Jon Dahl <jon@dira.no>
2023-10-16 15:50:29 +0900

    Improve the index mapping
    Greatly reduce program run time by condensing two maps into one.

--- basic_solver/hamiltonian.py
+++ basic_solver/hamiltonian.py
@@ -9,6 +9,23 @@ from parameters import clebsch_gordan, clebsch_gordan_array
 from tools import generate_indices
 from data_structures import Indices, timings
 
+# newmap = {
+#     0: [0, 1, 2, 3],
+#     1: [4, 5, 6, 7, 8, 9],
+#     2: [10, 11],
+#     3: [12, 13, 14, 15],
+#     4: [16, 17, 18, 19, 20, 21],
+#     5: [22, 23],
+# }
+orbital_idx_to_comp_m_idx_map = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7, 8, 9],
+    [10, 11],
+    [12, 13, 14, 15],
+    [16, 17, 18, 19, 20, 21],
+    [22, 23],
+]
+
 def calculate_onebody_matrix_element(
     interaction: Interaction,
     indices: Indices,
@@ -142,17 +159,20 @@ def twobody_annihilation_term(
     
     annihilation_results: list[tuple[float, list[int]]] = []
     
-    for annihilation_m_idx_0 in indices.orbital_idx_to_m_idx_map[annihilation_orb_idx_0]:
-        """
-        See the docstrings in
-        calculate_onebody_matrix_element for a
-        description on whats going on with these
-        indices.
-        """
-        annihilation_comp_m_idx_0 = indices.orbital_m_pair_to_composite_m_idx_map[(annihilation_orb_idx_0, annihilation_m_idx_0)]
+    # for annihilation_m_idx_0 in indices.orbital_idx_to_m_idx_map[annihilation_orb_idx_0]:
+    #     """
+    #     See the docstrings in
+    #     calculate_onebody_matrix_element for a
+    #     description on whats going on with these
+    #     indices.
+    #     """
+    #     annihilation_comp_m_idx_0 = indices.orbital_m_pair_to_composite_m_idx_map[(annihilation_orb_idx_0, annihilation_m_idx_0)]
+
+    for annihilation_comp_m_idx_0 in orbital_idx_to_comp_m_idx_map[annihilation_orb_idx_0]:
+        for annihilation_comp_m_idx_1 in orbital_idx_to_comp_m_idx_map[annihilation_orb_idx_1]:
         
-        for annihilation_m_idx_1 in indices.orbital_idx_to_m_idx_map[annihilation_orb_idx_1]:
-            annihilation_comp_m_idx_1 = indices.orbital_m_pair_to_composite_m_idx_map[(annihilation_orb_idx_1, annihilation_m_idx_1)]
+        # for annihilation_m_idx_1 in indices.orbital_idx_to_m_idx_map[annihilation_orb_idx_1]:
+        #     annihilation_comp_m_idx_1 = indices.orbital_m_pair_to_composite_m_idx_map[(annihilation_orb_idx_1, annihilation_m_idx_1)]
 
             # if annihilation_comp_m_idx_0 == annihilation_comp_m_idx_1: continue   # No change in program run time.
 
@@ -202,13 +222,14 @@ def twobody_creation_term(
     """
     creation_res: float = 0.0
 
-    for creation_m_idx_0 in indices.orbital_idx_to_m_idx_map[creation_orb_idx_0]:
-        creation_comp_m_idx_0 = indices.orbital_m_pair_to_composite_m_idx_map[(creation_orb_idx_0, creation_m_idx_0)]
+    # for creation_m_idx_0 in indices.orbital_idx_to_m_idx_map[creation_orb_idx_0]:
+    #     creation_comp_m_idx_0 = indices.orbital_m_pair_to_composite_m_idx_map[(creation_orb_idx_0, creation_m_idx_0)]
 
-        for creation_m_idx_1 in indices.orbital_idx_to_m_idx_map[creation_orb_idx_1]:
-            creation_comp_m_idx_1 = indices.orbital_m_pair_to_composite_m_idx_map[(creation_orb_idx_1, creation_m_idx_1)]
+    for creation_comp_m_idx_0 in orbital_idx_to_comp_m_idx_map[creation_orb_idx_0]:
+        for creation_comp_m_idx_1 in orbital_idx_to_comp_m_idx_map[creation_orb_idx_1]:
 
-            # if creation_comp_m_idx_0 == creation_comp_m_idx_1: continue # Actually increases program run time.
+        # for creation_m_idx_1 in indices.orbital_idx_to_m_idx_map[creation_orb_idx_1]:
+        #     creation_comp_m_idx_1 = indices.orbital_m_pair_to_composite_m_idx_map[(creation_orb_idx_1, creation_m_idx_1)]
 
             cg_creation = clebsch_gordan[(
                 indices.orbital_idx_to_j_map[creation_orb_idx_0],
